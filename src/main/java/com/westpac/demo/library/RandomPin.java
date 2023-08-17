0efcd91b6daf1997119bb5902306cf7b0086127f
attila.blazsek <attila.blazsek@lhsystems.com>
2023-08-17 03:07:11 +0200

    - Sets the result list elements must be unique  - add more test cases

--- src/main/java/com/westpac/demo/library/RandomPin.java
+++ src/main/java/com/westpac/demo/library/RandomPin.java
@@ -1,36 +1,42 @@
 package com.westpac.demo.library;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.experimental.UtilityClass;
+
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * The `randomPin` class provides methods for generating random PIN codes.
  */
-public class randomPin {
+@UtilityClass
+public class RandomPin {
+    static Random random = new Random();
+
     /**
      * Generates a random pin which has 4 digit
+     *
      * @return 4 digit length pin
      */
     public static String generatePin() {
-        Random random = new Random();
+
         return String.format("%04d", random.nextInt(10000));
     }
 
     /**
-     * Generates a specified number of random PIN codes, each consisting of 4 digits.
+     * Generates a specified number of random unique PIN codes, each consisting of 4 digits.
      *
      * @param count The number of random PIN codes to generate. Must be between 1 and 10000.
      * @return A list of generated PIN codes.
      * @throws IllegalArgumentException If the given count is not within the valid range.
      */
-    public static List<String> generatePins(int count) {
-        if (count < 1 || count > 10000) {
+    public static Set<String> generatePins(int count) {
+        if (count < 1 || count >= 10000) {
             throw new IllegalArgumentException("Count must be between 1 and 10000");
         }
 
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
+        Set<String> result = new HashSet<>();
+        while (result.size() < count) {
             result.add(generatePin());
         }
         return result;
--- src/test/java/com/westpac/demo/library/RandomPinTest.java
+++ src/test/java/com/westpac/demo/library/RandomPinTest.java
@@ -2,21 +2,31 @@ package com.westpac.demo.library;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
+import java.util.Set;
 
 
 class RandomPinTest {
 
     @Test
     void generatePin() {
-        String pin = randomPin.generatePin();
-        Assertions.assertEquals(4,pin.length());
+        String pin = RandomPin.generatePin();
+        Assertions.assertEquals(4, pin.length());
     }
 
-    @Test
-    void generatePins() {
-        List<String> pins = randomPin.generatePins(10);
-        Assertions.assertEquals(10,pins.size());
+    @ParameterizedTest
+    @ValueSource(ints = {1, 100, 300})
+    void generatePins(int number) {
+        Set<String> pins = RandomPin.generatePins(number);
+        Assertions.assertEquals(number, pins.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE,-1, 0, 10000, Integer.MAX_VALUE})
+    void generatePinsWithLessThenOne(int number) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> RandomPin.generatePins(number));
+        Assertions.assertEquals("Count must be between 1 and 10000", exception.getMessage());
     }
 }
